460fb43a83023e135fdacb9066548d5bd285c7fa
Sakshi Singh <sakshisingh.s1304@gmail.com>
2026-06-12 20:15:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 import plotly.express as px
 import plotly.graph_objects as go
 
